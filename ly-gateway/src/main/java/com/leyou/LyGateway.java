bc25d1804800bc7f38493c767184d21373e8b7e9
zhaoliang <zhaoliang@zqykj.com>
2019-11-05 17:18:16 +0800

    feat: add some common config

--- ly-gateway/src/main/java/com/leyou/LyGateway.java
+++ ly-gateway/src/main/java/com/leyou/LyGateway.java
@@ -2,9 +2,11 @@ package com.leyou;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringCloudApplication
+@EnableEurekaClient
 @EnableZuulProxy
 public class LyGateway {
     public static void main(String[] args) {
