6b29f3f14d0dc94c57d482e368f38b48cae41927
zhaoliang <zhaoliang@zqykj.com>
2019-11-09 16:46:05 +0800

    feat: 增加根据parent_id查询分类功能

---
+++ ly-item/ly-item-service/src/main/java/com/leyou/item/service/CategoryService.java
@@ -0,0 +1,25 @@
+package com.leyou.item.service;
+
+import com.leyou.item.mapper.CategoryMapper;
+import com.leyou.item.pojo.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryMapper categoryMapper;
+
+    public List<Category> queryListByParent(Long pid){
+        //mapper根据属性非空，作为sql查询条件
+        Category category = new Category();
+        category.setParentId(pid);
+
+        List<Category> categoryList = categoryMapper.select(category);
+
+        return categoryList;
+    }
+}
---
+++ ly-item/ly-item-service/src/main/java/com/leyou/item/web/CategoryController.java
@@ -0,0 +1,44 @@
+package com.leyou.item.web;
+
+import com.leyou.common.enums.ExceptionEnum;
+import com.leyou.common.exception.LyException;
+import com.leyou.item.pojo.Category;
+import com.leyou.item.service.CategoryService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("category")
+public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+
+    /**
+     * 根据父节点查询商品类目
+     * @param pid
+     * @return
+     */
+    @GetMapping("list")
+    public ResponseEntity<List<Category>> queryCategoryByPid(@RequestParam(value="pid",defaultValue = "0") Long pid){
+        log.info("根据parent_id查询商品,pid:{}",pid);
+        List<Category> categoryList = categoryService.queryListByParent(pid);
+
+        if (CollectionUtils.isEmpty(categoryList)){
+            throw new LyException(ExceptionEnum.CATEGORY_NOT_FOUND);
+        }
+
+        return ResponseEntity.ok(categoryList);
+
+    }
+
+
+}
