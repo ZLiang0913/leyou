044801c285abd969562627c81390fd69d0b1ac33
zhaoliang <zhaoliang@zqykj.com>
2019-11-09 16:45:17 +0800

    feat: 增加根据分页查询品牌查询功能

---
+++ ly-item/ly-item-service/src/main/java/com/leyou/item/mapper/BrandMapper.java
@@ -0,0 +1,8 @@
+package com.leyou.item.mapper;
+
+import com.leyou.item.pojo.Brand;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface BrandMapper extends tk.mybatis.mapper.common.Mapper<Brand> {
+}
---
+++ ly-item/ly-item-service/src/main/java/com/leyou/item/service/BrandService.java
@@ -0,0 +1,39 @@
+package com.leyou.item.service;
+
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.leyou.common.vo.PageResult;
+import com.leyou.item.mapper.BrandMapper;
+import com.leyou.item.pojo.Brand;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tk.mybatis.mapper.entity.Example;
+
+@Service
+public class BrandService {
+
+    @Autowired
+    private BrandMapper brandMapper;
+
+    public PageResult<Brand> queryBrandByPageAndSort(Integer page, Integer rows, String sortBy, Boolean desc, String key) {
+        //开始分页
+        PageHelper.startPage(page,rows);
+        //过滤
+        Example example = new Example(Brand.class);
+        //搜索关键字
+        if (StringUtils.isNotBlank(key)){
+            example.createCriteria().andLike("name","%"+key+"%").orEqualTo("letter",key);
+        }
+        //排序
+        if (StringUtils.isNotBlank(sortBy)){
+            String orderByClause = sortBy+ (desc ? " DESC":" ASC");
+            example.setOrderByClause(orderByClause);
+        }
+
+        //查询
+        Page<Brand> pageinfo = (Page<Brand>) this.brandMapper.selectByExample(example);
+
+        return new PageResult<Brand>(pageinfo.getTotal(),pageinfo);
+    }
+}
