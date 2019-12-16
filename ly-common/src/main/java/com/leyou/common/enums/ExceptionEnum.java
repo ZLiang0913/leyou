6521ae0d85e30e032879dce7ed11d0a2419770b4
ZLiang0913 <2581125848@qq.com>
2019-12-16 23:40:43 +0800

    feat: add item specification group

--- ly-common/src/main/java/com/leyou/common/enums/ExceptionEnum.java
+++ ly-common/src/main/java/com/leyou/common/enums/ExceptionEnum.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public enum ExceptionEnum {
     PRICE_CAN_NOT_BE_NULL(400,"价格不能为空"),
+    SPEC_GROUP_NOT_FOUND(404,"商品规格组不存在"),
     CATEGORY_NOT_FOUND(404,"商品分类没查到"),
     BRAND_CREATE_ERROR(500,"新增品牌失败")
     ;
---
+++ ly-item/ly-item-service/src/main/java/com/leyou/item/service/SpecificationService.java
@@ -0,0 +1,27 @@
+package com.leyou.item.service;
+
+import com.leyou.common.enums.ExceptionEnum;
+import com.leyou.common.exception.LyException;
+import com.leyou.item.mapper.SpecGroupMapper;
+import com.leyou.item.pojo.SpecGroup;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+import java.util.List;
+
+@Service
+public class SpecificationService {
+    @Autowired
+    private SpecGroupMapper specGroupMapper;
+
+    public List<SpecGroup> queryGroupByCid(Long cid) {
+        SpecGroup specGroup = new SpecGroup();
+        specGroup.setCid(cid);
+        List<SpecGroup> specGroupList = specGroupMapper.select(specGroup);
+
+        if (CollectionUtils.isEmpty(specGroupList)){
+            throw new LyException(ExceptionEnum.SPEC_GROUP_NOT_FOUND);
+        }
+        return specGroupList;
+    }
+}
