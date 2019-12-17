f39846ebc586c3775e14a8fe9b6e76ed71d1255d
zhaoliang <zhaoliang@zqykj.com>
2019-12-17 20:56:00 +0800

    feat: add tb_spec_param function

--- ly-item/ly-item-service/src/main/java/com/leyou/item/service/SpecificationService.java
+++ ly-item/ly-item-service/src/main/java/com/leyou/item/service/SpecificationService.java
@@ -3,16 +3,21 @@ package com.leyou.item.service;
 import com.leyou.common.enums.ExceptionEnum;
 import com.leyou.common.exception.LyException;
 import com.leyou.item.mapper.SpecGroupMapper;
+import com.leyou.item.mapper.SpecParamMapper;
 import com.leyou.item.pojo.SpecGroup;
+import com.leyou.item.pojo.SpecParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
+
 import java.util.List;
 
 @Service
 public class SpecificationService {
     @Autowired
     private SpecGroupMapper specGroupMapper;
+    @Autowired
+    private SpecParamMapper specParamMapper;
 
     public List<SpecGroup> queryGroupByCid(Long cid) {
         SpecGroup specGroup = new SpecGroup();
@@ -28,7 +33,7 @@ public class SpecificationService {
     public void saveGroup(SpecGroup specGroup) {
         int insert = specGroupMapper.insert(specGroup);
         if (insert!=1){
-            throw new LyException(ExceptionEnum.SPEC_REATE_ERROR);
+            throw new LyException(ExceptionEnum.SPEC_CREATE_ERROR);
         }
     }
 
@@ -38,5 +43,31 @@ public class SpecificationService {
 
     public void updateGroup(SpecGroup specGroup) {
         specGroupMapper.updateByPrimaryKey(specGroup);
+    }
+
+    public List<SpecParam> queryParamByGid(Long gid) {
+        SpecParam specParam = new SpecParam();
+        specParam.setGroupId(gid);
+        List<SpecParam> specParamList = specParamMapper.select(specParam);
+
+        if (CollectionUtils.isEmpty(specParamList)){
+            throw new LyException(ExceptionEnum.SPEC_PARAM_NOT_FOUND);
+        }
+        return specParamList;
+    }
+
+    public void saveGroupParam(SpecParam specParam) {
+        int insert = specParamMapper.insert(specParam);
+        if (insert!=1){
+            throw new LyException(ExceptionEnum.SPEC_PARAM_CREATE_ERROR);
+        }
+    }
+
+    public void deleteGroupParamById(Long id) {
+        specParamMapper.deleteByPrimaryKey(id);
+    }
+
+    public void updateGroupParam(SpecParam specParam) {
+        specParamMapper.updateByPrimaryKey(specParam);
     }
 }
--- ly-item/ly-item-service/src/main/java/com/leyou/item/web/SpecificationController.java
+++ ly-item/ly-item-service/src/main/java/com/leyou/item/web/SpecificationController.java
@@ -1,11 +1,20 @@
 package com.leyou.item.web;
 
 import com.leyou.item.pojo.SpecGroup;
+import com.leyou.item.pojo.SpecParam;
 import com.leyou.item.service.SpecificationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -26,7 +35,7 @@ public class SpecificationController {
      * @return
      */
     @PostMapping("group")
-    public ResponseEntity<Void> insertGroup(@RequestBody SpecGroup specGroup){
+    public ResponseEntity<Void> saveGroup(@RequestBody SpecGroup specGroup){
         specificationService.saveGroup(specGroup);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
@@ -50,6 +59,34 @@ public class SpecificationController {
     @PutMapping("group")
     public ResponseEntity<Void> updateGroup(@RequestBody SpecGroup specGroup){
         specificationService.updateGroup(specGroup);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    /**
+     * 根据组id查询参数
+     * @param gid
+     * @return
+     */
+    @GetMapping("params")
+    public ResponseEntity<List<SpecParam>>  queryParamByGid(@RequestParam("gid") Long gid){
+        return ResponseEntity.ok(specificationService.queryParamByGid(gid));
+    }
+
+    @PostMapping("param")
+    public ResponseEntity<Void> saveGroupParam(@RequestBody SpecParam specParam){
+        specificationService.saveGroupParam(specParam);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping("param/{id}")
+    public ResponseEntity<Void> deleteGroupParamById(@PathVariable("id") Long id){
+        specificationService.deleteGroupParamById(id);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @PutMapping("param")
+    public ResponseEntity<Void> updateGroupParam(@RequestBody SpecParam specParam){
+        specificationService.updateGroupParam(specParam);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
